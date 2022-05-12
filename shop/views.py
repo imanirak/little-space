3c042c4421d37f60db1b9498ba958f845550c24f
imanirak <karinadgarces@gmail.com>
2022-05-11 22:44:57 -0400

    base setup

---
+++ shop/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View 
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+
+
+
+class Home(TemplateView):
+    template_name='home.html'
+    
+    def login_page(request):
+        context = {
+            'login': login,
+        }
+    
+        return render(request, context)
