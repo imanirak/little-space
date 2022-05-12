5bf6ab5b2ce58d0d6a43d66e7ef7cf3973b94187
imanirak <karinadgarces@gmail.com>
2022-05-12 10:47:55 -0400

    shop views

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home")
+    path('', views.Home.as_view(), name="home"),
+     #SHOP
+    path('shop/new', views.Shop_Create.as_view(), name="shop_create"),
+    path('shop/', views.Shop_List.as_view(), name="shop_list"),
+    path('shop/<int:pk>/', views.Shop_Detail.as_view(), name="shop_detail"),
+    path('shop/<int:pk>/update', views.Shop_Update.as_view(), name="shop_update"),
+    path('shop/<int:pk>/delete', views.Shop_Delete.as_view(), name="shop_delete"),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views import View 
-from django.http import HttpResponse
-from django.views.generic.base import TemplateView
+from django.http import HttpResponseRedirect
+from django.views.generic.base import TemplateView, CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth import authenticate, login, logout
 # Create your views here.
-
-
+from models import User, Shop, Item, Inventory
+from django.urls import reverse
 
 class Home(TemplateView):
     template_name='home.html'
@@ -18,3 +18,53 @@ class Home(TemplateView):
         return render(request, context)
     
     
+#SHOP VIEWS
+class ShopCreate(CreateView):
+  model = Shop
+  fields = '__all__'
+  template_name='shop_create.html'
+
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.user = self.request.user
+    self.object.save()
+    return HttpResponseRedirect('/shops/new')
+
+
+
+class Shop_List(TemplateView):
+    template_name='shop_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get('name')
+    
+
+        if name != None:
+                    context['shop']=Shop.objects.filter(name__icontains=name)
+                    context['header']= f'Searching for {name}'
+        else:
+                    context['shops']=Shop.objects.all()
+                    context['items']=Item.objects.all()
+            
+        context['header']= 'Shops:'
+            
+        return 
+    
+   
+class Shop_Detail(DetailView):
+    model = Shop
+    template_name = "shop_detail.html"
+    
+
+class Shop_Update(UpdateView):
+    model = Shop
+    fields = '__all__'
+    template_name = "shop_update.html"
+    def get_success_url(self):
+        return reverse('shop_detail', kwargs={'pk': self.object.pk})
+
+class Shop_Delete(DeleteView):
+    model = Shop
+    template_name = 'shop_delete.html'
+    success_url = "/shop/"
